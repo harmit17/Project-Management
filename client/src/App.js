756ab6b7991a3913d3451611231bd0823a7ceb53
Harrmit Chauhan <harmitchauhan1@gmail.com>
2023-10-16 15:11:20 -0400

    added Project management project with CRUD functionalities

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Header from "./components/Header";
-import Clients from "./components/Clients";
 import { ApolloProvider, InMemoryCache, ApolloClient } from '@apollo/client';
-import AddClientModal from "./components/AddClientModal";
-
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Project from './pages/Project';
 
 const cache = new InMemoryCache({
 
@@ -34,11 +35,16 @@ function App() {
   return (
     <>
     <ApolloProvider client={client}>
-      <Header/>
+      <Router>
+        <Header/>
         <div className="container">
-          <AddClientModal/>
-          <Clients/>
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/projects/:id' element={<Project/>}/>
+            <Route path='*' element={<NotFound/>} />
+          </Routes>
         </div>
+      </Router>
       </ApolloProvider>
     </>
   );
---
+++ client/src/pages/Project.jsx
@@ -0,0 +1,43 @@
+import { Link, useParams } from 'react-router-dom';
+import Spinner from '../components/Spinner';
+import { useQuery } from '@apollo/client';
+import ClientInfo from '../components/ClientInfo';
+import EditProjectForm from '../components/EditProjectForm';
+import { GET_PROJECT, GET_PROJECTS } from '../queries/projectQueries';
+import DelectProjectButton from '../components/DelectProjectButton';
+
+
+export default function Project() {
+
+    const { id } = useParams();
+    const { loading, error, data } = useQuery( GET_PROJECT, {
+
+        variables: { id } 
+    } );
+
+    if (loading) return <Spinner/>;
+    if (error) return <p>Something went wrong</p>
+
+    return (
+        <>
+            { !loading && !error && (
+                <div className="mx-auto w-75 card p-5">
+                    <Link to="/" className='btn btn-light btn-sm w-25 d-inline ms-auto'>
+                        Back
+                    </Link>
+                    <h1>{ data.project.name }</h1>
+                    <p>{ data.project.description }</p>
+                    <h5 className='mt-3'>Project Status</h5>
+                    <p className='lead'>{ data.project.status }</p>
+
+                    <ClientInfo client = { data.project.client } />
+
+                    <EditProjectForm project={data.project}/>
+
+                    <DelectProjectButton projectId={data.project.id}/>
+                </div>
+            )}
+        
+        </>
+    )
+}
